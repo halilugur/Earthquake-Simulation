5df7e7d86e39f93478eb5e434230d2e856f0aedc
halilugur <mr.halilugur@gmail.com>
2021-12-26 16:58:32 +0300

    change expected threshold.

--- App.js
+++ App.js
@@ -32,26 +32,35 @@ class App extends Component {
       if (countOfAppUse === 0 || countOfSendToCloud === 0) {
         Alert.alert('Warning (Uyarı)', 'Please fill required fields.\nLütfen gerekli alanları doldurun.')
       } else {
-        this.setState({disabled: true})
-        fetch('https://ugurhalil.com/wp-json/earthquake/add', {
-          method: 'POST',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            count_of_app_use: countOfAppUse,
-            count_of_send_to_cloud: countOfSendToCloud,
-            is_read_by_device: false
+        if (this.checkUseThreshold(countOfSendToCloud, 0, countOfAppUse, 0, 100)) {
+          this.setState({disabled: true})
+          fetch('https://ugurhalil.com/wp-json/earthquake/add', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              count_of_app_use: countOfAppUse,
+              count_of_send_to_cloud: countOfSendToCloud,
+              is_read_by_device: false
+            })
+          }).then(result => {
+            result.status === 200 ? Alert.alert('Success (Başarılı)', 'Data sent.\nVeri gönderildi.') :
+              Alert.alert('Fail (Başarısız)', 'Data could not be sent.\nVeri gönderilemedi.')
+            this.setState({
+              countOfAppUse: 0,
+              countOfSendToCloud: 0,
+              disabled: false
+            })
           })
-        }).then(result => {
-          result.status === 200 ? Alert.alert('Success (Başarılı)', 'Data sent.\nVeri gönderildi.') :
-            Alert.alert('Fail (Başarısız)', 'Data could not be sent.\nVeri gönderilemedi.')
-          this.setState({
-            countOfAppUse: 0,
-            countOfSendToCloud: 0,
-            disabled: false
-          })
-        })
+        } else {
+          Alert.alert('Warning (Uyarı)', 'Data could not be sent because the threshold value could not be exceeded (expected %70).\nEşik değeri aşılamadığından veri gönderilemedi (Beklenen %70).')
+        }
       }
     }
+  }
+
+  checkUseThreshold = (x:number, in_min:number, in_max:number, out_min:number, out_max:number) => {
+    let mapTo = ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
+    return mapTo >= 70
   }
 
   render() {
